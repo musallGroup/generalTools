a4469ed096b417dc6084198797312b5293baee81
Simon Musall <49614434+musall@users.noreply.github.com>
2022-04-25 19:47:31 +0200

    added some functions and updated dorsalMaps

--- imageScale.m
+++ imageScale.m
@@ -1,12 +1,23 @@
-function [mapImg, cRange] = imageScale(cMap, cRange)
+function [mapImg, cRange] = imageScale(cMap, cRange, twoSided)
 % quick command to plot image and set NaNs to be transparent. cRange
 % defines a symmetric color range, based on top 'cRange' percentile in the image.
-% usage: [mapImg, cRange] = imageScale(cMap, cRange)
+% 'twoSided' determines whether color range should be positive and negative
+% (default) or only positive.
+% usage: [mapImg, cRange] = imageScale(cMap, cRange, twoSided)
 
-if ~exist('cRange', 'var')
+if ~exist('cRange', 'var') || isempty(cRange)
     cRange = abs(prctile(cMap(:),97.5));
 end
 
-cRange = [-cRange cRange];
+if ~exist('twoSided', 'var')
+    twoSided = true;
+end
+
+if twoSided
+    cRange = [-cRange cRange];
+else
+    cRange = [0 cRange];
+end
+
 mapImg = imshow(squeeze(cMap),cRange);
 set(mapImg,'AlphaData',~isnan(mapImg.CData)); %make NaNs transparent.
