e406fd2872a633ef8fff53161c6944d138eeda59
NabbefeldG <68555047+NabbefeldG@users.noreply.github.com>
2022-10-12 11:46:29 +0200

    renamed stuff to be more similar to standard matlab function

--- Playground/Gerion/test_binomial_difference.m
+++ Playground/Gerion/test_binomial_difference.m
@@ -1,8 +1,8 @@
 function [h, Z, p] = test_binomial_difference(p1, p2, n1, n2, alpha, side)
-    % side: 'two' for both tails otherwise one sided.
+    % side: 'both' for both tails otherwise one sided.
     
     if ~exist('alpha', 'var'); alpha = 0.05; end
-    if ~exist('side', 'var'); side = 'two'; end
+    if ~exist('side', 'var'); side = 'both'; end
     
     if isnan(p1) || isnan(p2) || (n1 < 1) || (n2 < 1)
         h = nan; Z = nan; p = nan;
@@ -12,14 +12,14 @@ function [h, Z, p] = test_binomial_difference(p1, p2, n1, n2, alpha, side)
     p_hat = (n1*p1 + n2*p2) / (n1+n2);
     Z = (p1-p2) / sqrt(p_hat * (1 - p_hat) * ((1 / n1) + (1 / n2)));
 
-    if strcmp(side, 'two')
+    if strcmp(side, 'two') || strcmp(side, 'both')
         % the (1-normcdf(Z)) if cause matlab apparently returns the percentile
         % instead of the p-value
-        p = (1 - normcdf(abs(Z))) ./ 2;  % two-sided
+        p = 2 .* normcdf(abs(Z), 'upper');  % two-sided
     elseif strcmp(side, 'left')
-        p = normcdf(abs(Z));  % one-sided
+        p = 1 - normcdf(abs(Z), 'upper');  % one-sided
     elseif strcmp(side, 'right')
-        p = 1 - normcdf(abs(Z));  % one-sided
+        p = normcdf(abs(Z), 'upper');  % one-sided
     else
         error('Unclear tail!');
     end
