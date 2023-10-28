b8dfd368c5e9839c686f5f53d6a3bcdb7c3c5f95
Simon Musall <49614434+musall@users.noreply.github.com>
2023-10-28 20:43:06 +0200

    Create smoothImg.m

---
+++ smoothImg.m
@@ -0,0 +1,36 @@
+function DataOut = smoothImg(DataIn,type,fLength,sigma)
+% Apply either gaussian or box filter to a 2D image. 
+% Usage: DataOut = smoothImg(DataIn,type,fLength,sigma)
+% Input:    DataIn = 2D matrix on which the filter should be applied
+%           type: type of filter. 1 for gaussian, 2 for box filter
+%           fLength = filter length.
+%           sigma = sigma of the gaussian kernel. Standard is 1.76 for a single standard deviation.
+% Output:   DataOut = filtered version of DataIn.
+
+if ~exist('type','var') || isempty(type) || strcmpi(type,'gaussian')
+    type = 1; %use gaussian filter by default
+elseif strcmpi(type,'box')
+    type = 2; %use box
+end
+
+if ~exist('fLength','var') || isempty(fLength)
+    fLength = 3;
+end
+
+if ~exist('sigma','var') || isempty(sigma)
+    sigma = 1.76;
+end
+
+if fLength > 1
+    if type == 1
+        Kernel = ones(fLength,fLength); % Create box filter based on flength
+        Kernel = Kernel ./ fLength^2;
+    elseif type == 2
+        [x,y]=meshgrid(-fLength:fLength,-fLength:fLength); % create gaussian filter based on flength and sigma
+        Kernel= exp(-(x.^2+y.^2)/(2*sigma*sigma))/(2*pi*sigma*sigma);
+    end
+    DataOut = conv2(double(DataIn), Kernel, 'same'); %convolve original matrix with filter
+else
+    DataOut = DataIn; %don't apply filter if fLength is equal or lower then 1
+end
+% DataOut = padarray(double(DataOut),[1 1],NaN,'both'); %add NaNs to image to see its edge against zero-padded background when rotating.
