6a1d8a2c603d93c01fbb9f03bfc241180bf2dd32
Simon Musall <49614434+musall@users.noreply.github.com>
2023-12-14 10:31:43 +0100

    Update LME_compare.m

--- LME_compare.m
+++ LME_compare.m
@@ -1,4 +1,4 @@
-function modelCompare = LME_compare(dataIn1, dataIn2, randomVar)
+function [pVal_cStim, tStat_cStim, fullmodel, modelCompare] = LME_compare(dataIn1, dataIn2, randomVar)
 % function to compare to variables, while controlling for the impact of the
 % random variable 'randomVar'. The likelihood ratio test statistics LRStat 
 % is indicative of whether dataIn2 has an impact on dataIn1, when
@@ -15,8 +15,12 @@ cGroups = cGroups(:);
 % combine into one table and create model
 tbl = table(cData, cStims, cGroups, 'VariableNames',{'cData','cStims','cGroups'});
 fullmodel = fitlme(tbl,'cData ~ cStims + (1 |cGroups)');
-nullmodel = fitlme(tbl,'cData ~ 1 + (1|cGroups)');
+pVal_cStim = fullmodel.Coefficients.pValue(2); %return p-Value for stimulus regressor in full model
+tStat_cStim = fullmodel.Coefficients.tStat(2); %return p-Value for stimulus regressor in full model
 
-% Test the significance of the fixed effect 'cStims'
-% This will provide a p-value indicating the significance of the impact of 'cStims' on 'cData'
-modelCompare = compare(nullmodel, fullmodel, 'CheckNesting',true);
+if nargout > 3
+    % Test the significance of the fixed effect 'cStims'
+    % This will provide a p-value indicating the significance of the impact of 'cStims' on 'cData'
+    nullmodel = fitlme(tbl,'cData ~ 1 + (1|cGroups)');
+    modelCompare = compare(nullmodel, fullmodel, 'CheckNesting',true);
+end
