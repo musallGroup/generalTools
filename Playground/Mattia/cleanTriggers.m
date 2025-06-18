a667550cdaa4dff61a30e79e59fe4b82979e6eba
mchini <37143108+mchini@users.noreply.github.com>
2025-06-18 13:18:46 +0200

    minor

--- Playground/Mattia/cleanTriggers.m
+++ Playground/Mattia/cleanTriggers.m
@@ -1,8 +1,14 @@
-function VideoData = cleanTriggers(VideoData, SessionData)
+function VideoData = cleanTriggers(VideoData, SessionData, keep)
 
 % find the indeces of the valid trials
 idx = find([SessionData.Response_left] ~= -1 & ...
     [SessionData.auto_reward] ~= 1 & ...
     [SessionData.both_spouts] == 1);
-% only keep the respective triggers
-VideoData.vidDigitalTrigs = VideoData.vidDigitalTrigs(idx); %#ok<*FNDSB>
+% only keep the respective triggers but adjust depending on which variable
+% has the correct amount of triggers
+if keep == 1
+    VideoData.vidDigitalTrigs = VideoData.vidDigitalTrigs(idx); %#ok<*FNDSB>
+elseif keep == 2
+    VideoData.vidDigitalTrigs = VideoData.useTrigs{1, 1};
+    VideoData.vidDigitalTrigs = VideoData.vidDigitalTrigs(idx); %#ok<*FNDSB>
+end
